b310c290045d0242d41cd799267f0e2e9b610620
simon <wanwanzainaer@gmail.com>
2021-01-24 15:00:09 -0800

    commit

--- app.ts
+++ app.ts
@@ -1,9 +1,28 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  name: 'Simon',
-  age: 30,
-};
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
 
-console.log(person);
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+// let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});

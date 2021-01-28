ca3a6ce169f76677f0bdba38d94311b9e3f66a8d
simon <wanwanzainaer@gmail.com>
2021-01-27 23:18:41 -0800

    Add commit for github

---
+++ dist/app.d.ts
@@ -0,0 +1,2 @@
+declare const add: (a: number, b: number) => number;
+declare const printOutput: (a: number | string) => void;
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
-console.log('test --wtach mdoe');
+// const userName = 'Max';
 
-const button = document.querySelector('button')!;
+// let age = 30;
 
-button.addEventListener('click', () => {
-  console.log('Clicked!!');
-});
+// age = 29;
+
+// if (age > 20) {
+//   let isOdd = true;
+// }
+
+// const add = (a: number, b: number) => {
+//   return a + b;
+// };
+
+// console.log(add(2, 5));
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+printOutput(add(5, 2));
